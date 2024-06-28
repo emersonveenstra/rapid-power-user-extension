1b1aeb1a192c69947e09522ec2c2d8f70e6a9f01
emerson <git@emersonveenstra.net>
2024-06-28 08:31:24 -0400

    don't sync screenshots

--- sync-shared.sh
+++ sync-shared.sh
@@ -1,3 +1,3 @@
-rm -rf chrome/shared/{icons,screenshots,scripts,static_rules.json,sw.js} && cp -r shared/{icons,screenshots,scripts,static_rules.json,sw.js} chrome/
-rm -rf firefox/shared/{icons,screenshots,scripts,static_rules.json,sw.js} && cp -r shared/{icons,screenshots,scripts,static_rules.json,sw.js} firefox/
-rm -rf local/shared/{icons,screenshots,scripts,static_rules.json,sw.js} && cp -r shared/{icons,screenshots,scripts,static_rules.json,sw.js} local/
+rm -rf chrome/shared/{iconsscripts,static_rules.json,sw.js} && cp -r shared/{icons,scripts,static_rules.json,sw.js} chrome/
+rm -rf firefox/shared/{iconsscripts,static_rules.json,sw.js} && cp -r shared/{icons,scripts,static_rules.json,sw.js} firefox/
+rm -rf local/shared/{icons,scripts,static_rules.json,sw.js} && cp -r shared/{icons,scripts,static_rules.json,sw.js} local/
